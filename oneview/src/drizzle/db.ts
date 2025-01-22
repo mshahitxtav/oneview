4bf742341b9fe1a4a5262a53327a6df5647256dd
v60843 <mshahi@txtav.com>
2025-01-22 14:27:31 -0600

    fix env variable name

--- oneview/src/drizzle/db.ts
+++ oneview/src/drizzle/db.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const client = postgres(process.env['DATABASEURL'] ?? '');
+const client = postgres(process.env['DATABASE_URL'] ?? '');
+
 export const db = drizzle(client);
--- oneview/src/server/trpc/trpc.ts
+++ oneview/src/server/trpc/trpc.ts
@@ -1,6 +1,6 @@
 import { initTRPC } from '@trpc/server';
 import { Context } from './context';
-import SuperJSON from 'superjson';
+import { SuperJSON } from 'superjson';
 
 const t = initTRPC.context<Context>().create({
   transformer: SuperJSON,
